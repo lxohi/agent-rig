8225b29afa9ecc039c5dfa7f093a07aba77f4987
lxohi <16010388+lxohi@users.noreply.github.com>
2026-02-02 19:32:00 +0800

    chore: bump version to 0.3.0

--- src/index.tsx
+++ src/index.tsx
@@ -39,7 +39,7 @@ checkAndSwap().then((result) => {
 program
   .name('arig')
   .description('CLI tool for creating isolated development environments for coding agents')
-  .version('0.2.4');
+  .version('0.3.0');
 
 program
   .command('list')
